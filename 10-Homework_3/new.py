b33b7ef7b690696882f9acb66db9b23c9fdf64d8
adampietrusiewicz <ap132003@student.sgh.waw.pl>
2023-12-03 23:54:03 +0100

    homework done

--- 10-Homework_3/new.py
+++ 10-Homework_3/new.py
@@ -1,1 +1,11 @@
-pip install matplotlib seaborn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+insurance = pd.read_csv('insurance.csv')
+insurance.head()
+insurance.info()
+insurance.describe()
+insurance.columns
